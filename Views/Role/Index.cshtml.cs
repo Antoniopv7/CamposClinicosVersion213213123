fadb9f566ed3d4f2f1865b10e490a4ee2453f1cb
Antonio de Jesus Perez Vicencio <71039689+Antoniopv7@users.noreply.github.com>
2024-10-08 00:17:18 -0300

    vistas

---
+++ Views/Role/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Gestion_Del_Presupuesto.Views.Role
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
